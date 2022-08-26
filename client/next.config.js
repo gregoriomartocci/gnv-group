d1fb6a055d67dd1d43f0838c2562e2ba2b8a34f1
gregoriomartocci <gregoriomartocci@gmail.com>
2022-08-26 17:34:39 -0300

    ok

--- client/next.config.js
+++ client/next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   pageExtensions: ["page.tsx", "page.ts", "page.jsx", "page.js"],
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
