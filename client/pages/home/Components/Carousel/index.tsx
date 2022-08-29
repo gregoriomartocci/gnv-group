5ab1b262088007a363446367b5abc8d87868fef7
gregoriomartocci <gregoriomartocci@gmail.com>
2022-08-29 19:44:40 -0300

    Ok

--- client/pages/home/Components/Carousel/index.tsx
+++ client/pages/home/Components/Carousel/index.tsx
@@ -52,13 +52,11 @@ const UseCarousel = ({ items, slideTime }: TUseCarousel) => {
     },
   };
 
-  console.log(height, width, "ok");
-
   // We only have 3 images, but we paginate them absolutely (ie 1, 2, 3, 4, 5...) and
   // then wrap that within 0-2 to find our image ID in the array below. By passing an
   // absolute page index as the `motion` component's `key` prop, `AnimatePresence` will
   // detect it as an entirely new image. So you can infinitely paginate as few as 1 images.
-  const imageIndex = wrap(0, items?.length ?? 0, page);
+  const imageIndex = wrap(0, items?.length, page);
 
   const paginate = (newDirection: number) => {
     setPage([page + newDirection, newDirection]);
@@ -70,127 +68,133 @@ const UseCarousel = ({ items, slideTime }: TUseCarousel) => {
   };
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        position: "relative",
-        width: "100%",
-        height: "100%",
-        zIndex: 1,
-      }}
-    >
-      {getFormat(items[imageIndex]?.src) === "MP4" ? (
-        <AnimatePresence initial={false} custom={direction}>
-          <motion.video
-            style={{
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              cursor: "pointer",
-            }}
-            autoPlay
-            loop
-            muted
-            key={page}
-            src={items[imageIndex]?.src}
-            custom={direction}
-            variants={variants}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            transition={{
-              x: { type: "spring", stiffness: 300, damping: 30 },
-              opacity: { duration: 0.2 },
-            }}
-            drag="x"
-            dragConstraints={{ left: 0, right: 0 }}
-            dragElastic={1}
-            onDragEnd={(e, { offset, velocity }) => {
-              const swipe = swipePower(offset.x, velocity.x);
-              if (swipe < -swipeConfidenceThreshold) {
-                paginate(1);
-              } else if (swipe > swipeConfidenceThreshold) {
-                paginate(-1);
-              }
-            }}
-          />
-        </AnimatePresence>
-      ) : (
-        <AnimatePresence initial={false} custom={direction}>
-          <motion.img
-            style={{
-              position: "absolute",
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-              cursor: "pointer",
-            }}
-            key={page}
-            src={items[imageIndex].src}
-            custom={direction}
-            variants={variants}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            transition={{
-              x: { type: "spring", stiffness: 300, damping: 30 },
-              opacity: { duration: 0.2 },
-            }}
-            drag="x"
-            dragConstraints={{ left: 0, right: 0 }}
-            dragElastic={1}
-            onDragEnd={(e, { offset, velocity }) => {
-              const swipe = swipePower(offset.x, velocity.x);
-              if (swipe < -swipeConfidenceThreshold) {
-                paginate(1);
-              } else if (swipe > swipeConfidenceThreshold) {
-                paginate(-1);
-              }
-            }}
-          />
-        </AnimatePresence>
-      )}
+    <React.Fragment>
+      {items?.length ? (
+        <Box
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            position: "relative",
+            width: "100%",
+            height: "100%",
+            zIndex: 1,
+          }}
+        >
+          {getFormat(items[imageIndex]?.src) === "MP4" ? (
+            <AnimatePresence initial={false} custom={direction}>
+              <motion.video
+                style={{
+                  position: "absolute",
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  cursor: "pointer",
+                }}
+                autoPlay
+                loop
+                muted
+                key={page}
+                src={items[imageIndex]?.src}
+                custom={direction}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  x: { type: "spring", stiffness: 300, damping: 30 },
+                  opacity: { duration: 0.2 },
+                }}
+                drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                dragElastic={1}
+                onDragEnd={(e, { offset, velocity }) => {
+                  const swipe = swipePower(offset.x, velocity.x);
+                  if (swipe < -swipeConfidenceThreshold) {
+                    paginate(1);
+                  } else if (swipe > swipeConfidenceThreshold) {
+                    paginate(-1);
+                  }
+                }}
+              />
+            </AnimatePresence>
+          ) : (
+            <AnimatePresence initial={false} custom={direction}>
+              <motion.img
+                style={{
+                  position: "absolute",
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                  cursor: "pointer",
+                }}
+                key={page}
+                src={items[imageIndex].src}
+                custom={direction}
+                variants={variants}
+                initial="enter"
+                animate="center"
+                exit="exit"
+                transition={{
+                  x: { type: "spring", stiffness: 300, damping: 30 },
+                  opacity: { duration: 0.2 },
+                }}
+                drag="x"
+                dragConstraints={{ left: 0, right: 0 }}
+                dragElastic={1}
+                onDragEnd={(e, { offset, velocity }) => {
+                  const swipe = swipePower(offset.x, velocity.x);
+                  if (swipe < -swipeConfidenceThreshold) {
+                    paginate(1);
+                  } else if (swipe > swipeConfidenceThreshold) {
+                    paginate(-1);
+                  }
+                }}
+              />
+            </AnimatePresence>
+          )}
 
-      <Box
-        sx={{
-          display: "flex",
-          justifyContent: "flex-start",
-          alignItems: "center",
-          zIndex: 1,
-          padding: "0% 10%",
-        }}
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            left: "50px",
-            zIndex: 1,
-            cursor: "pointer",
-          }}
-          component="span"
-          onClick={() => paginate(-1)}
-        >
-          <ArrowBackIosIcon sx={{ color: "#fff", fontSize: "30px" }} />
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "flex-start",
+              alignItems: "center",
+              zIndex: 1,
+              padding: "0% 10%",
+            }}
+          >
+            <Box
+              sx={{
+                position: "absolute",
+                left: "50px",
+                zIndex: 1,
+                cursor: "pointer",
+              }}
+              component="span"
+              onClick={() => paginate(-1)}
+            >
+              <ArrowBackIosIcon sx={{ color: "#fff", fontSize: "30px" }} />
+            </Box>
+            <Box sx={{ marginBottom: "10px" }}>
+              <Typography sx={HeaderTitle}>
+                {items[imageIndex]?.phrase}
+              </Typography>
+            </Box>
+            <Box
+              sx={{
+                position: "absolute",
+                right: "50px",
+                zIndex: 1,
+                cursor: "pointer",
+              }}
+              component="span"
+              onClick={() => paginate(1)}
+            >
+              <ArrowForwardIosIcon sx={{ color: "#fff", fontSize: "30px" }} />
+            </Box>
+          </Box>
         </Box>
-        <Box sx={{ marginBottom: "10px" }}>
-          <Typography sx={HeaderTitle}>{items[imageIndex].phrase}</Typography>
-        </Box>
-        <Box
-          sx={{
-            position: "absolute",
-            right: "50px",
-            zIndex: 1,
-            cursor: "pointer",
-          }}
-          component="span"
-          onClick={() => paginate(1)}
-        >
-          <ArrowForwardIosIcon sx={{ color: "#fff", fontSize: "30px" }} />
-        </Box>
-      </Box>
-    </Box>
+      ) : null}
+    </Fragment>
   );
 };
 
--- client/pages/ventures/components/Venture/index.tsx
+++ client/pages/ventures/components/Venture/index.tsx
@@ -2,6 +2,7 @@ import { Box } from "@mui/material";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import parse from "html-react-parser";
 import { SxProps, Theme } from "@mui/material";
+import { Fragment } from "react";
 
 const CardContainer: SxProps<Theme> = {
   display: "flex",
@@ -32,72 +33,76 @@ const CardBody: SxProps<Theme> = {
   fontFamily: "'Poppins', sans-serif",
 };
 
-const Venture = ({ images, name, status, description }: any) => {
+const Venture = (item: any) => {
   const santize = (string: string) => {
     const reactElement = parse(string);
     return reactElement;
   };
 
   return (
-    <Box sx={CardContainer}>
-      <img src={images[0]?.src ?? ""} alt={name} />
-      <Box sx={CardHeader}>
-        <span
-          style={{
-            color: "#212121",
-            fontWeight: 600,
-            fontSize: "30px",
-            margin: "15px 0 0 0",
-          }}
-        >
-          {name}
-        </span>
-      </Box>
+    <Fragment>
+      {item ? (
+        <Box sx={CardContainer}>
+          <img src={item?.images[0]?.src ?? ""} alt={item?.name ?? ""} />
+          <Box sx={CardHeader}>
+            <span
+              style={{
+                color: "#212121",
+                fontWeight: 600,
+                fontSize: "30px",
+                margin: "15px 0 0 0",
+              }}
+            >
+              {item?.name ?? ""}
+            </span>
+          </Box>
 
-      <Box sx={CardBody}>
-        <Box
-          style={{
-            display: "flex",
-            alignItems: "center",
-            margin: "5px 0 0 0",
-          }}
-        >
-          <span
-            style={{
-              color: "#212121",
-              fontWeight: 700,
-              fontSize: "16px",
-            }}
-          >
-            {status.toUpperCase()}
-          </span>
-        </Box>
+          <Box sx={CardBody}>
+            <Box
+              style={{
+                display: "flex",
+                alignItems: "center",
+                margin: "5px 0 0 0",
+              }}
+            >
+              <span
+                style={{
+                  color: "#212121",
+                  fontWeight: 700,
+                  fontSize: "16px",
+                }}
+              >
+                {item?.status.toUpperCase() ?? ""}
+              </span>
+            </Box>
 
-        <Box
-          style={{
-            color: "#757575",
-            fontWeight: 500,
-            fontSize: "17px",
-            margin: "10px 0 0 0",
-          }}
-        >
-          {santize(description ?? "")}
+            <Box
+              style={{
+                color: "#757575",
+                fontWeight: 500,
+                fontSize: "17px",
+                margin: "10px 0 0 0",
+              }}
+            >
+              {santize(item?.description ?? "")}
+            </Box>
+            <Box
+              style={{
+                display: "flex",
+                alignItems: "center",
+                color: "#212121",
+                fontWeight: 600,
+                fontSize: "17px",
+                margin: "10px 0 0 0",
+              }}
+            >
+              Ver Proyecto
+              <KeyboardArrowRightIcon />
+            </Box>
+          </Box>
         </Box>
-        <Box
-          style={{
-            display: "flex",
-            alignItems: "center",
-            color: "#212121",
-            fontWeight: 600,
-            fontSize: "17px",
-            margin: "10px 0 0 0",
-          }}
-        >
-          Ver Proyecto
-          <KeyboardArrowRightIcon />
-        </Box>
-      </Box>
-    </Box>
+      ) : null}
+    </Fragment>
   );
 };
 
