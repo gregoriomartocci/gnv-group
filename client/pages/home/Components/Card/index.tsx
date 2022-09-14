dc88f7249c3800a60bf7d20de4acf8d83d78dcbd
gregoriomartocci <gregoriomartocci@gmail.com>
2022-09-14 10:12:47 -0300

    ok

--- client/pages/home/Components/Card/index.tsx
+++ client/pages/home/Components/Card/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import { TDemo } from "../..";
@@ -6,8 +6,20 @@ import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import Link from "next/link";
 
 const Card = ({ img, title }: TDemo) => {
+  const [hover, setHover] = useState<boolean>(false);
+
+  const handleMouseEnter = () => {
+    setHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHover(false);
+  };
+
   return (
     <Box
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
       sx={{
         position: "relative",
         display: "flex",
@@ -27,10 +39,10 @@ const Card = ({ img, title }: TDemo) => {
           width: "100%",
           height: "100%",
           objectFit: "cover",
-          objectPosition: { xs: " 75% 0%", sm: "", md: "", lg: "", xl: "" },
+          objectPosition: { xs: " 75% 0%" },
           transition: "all 1.25s ease",
 
-          "&:hover": {
+          "&:hover ": {
             transition: "all 1.25s ease",
             transform: "scale(1.05)",
           },
@@ -42,108 +54,63 @@ const Card = ({ img, title }: TDemo) => {
       <Box
         sx={{
           display: "flex",
-          position: "relative",
-          top: 0,
+          alignItems: "center",
+          position: "absolute",
           left: 0,
           width: "100%",
-          height: "100%",
           userSelect: "none",
           flexDirection: "column",
-          justifyContent: "flex-end",
-          background: "rgba(0,0,0,0.15)",
-          alignItems: "center",
-          pointerEvents: "none",
+          justifyContent: "flex-start",
+          pointerEvents: "auto",
+          height: "25%",
           padding: "65px",
           color: "#fff",
           fontFamily: "'Poppins', sans-serif",
+          background: "rgba(255,255,255,0)",
+          backdropFilter: "blur(7px)",
+          boxShadow: "0 8px 32px rgba(31,38,135, 0.37)",
+
+          bottom: `${hover ? 0 : "-25%"}`,
+          transition: "all 1.25s ease",
+          transform: "scale(1.05)",
         }}
       >
-        <Box
+        <Typography
           sx={{
-            display: "flex",
-            alignItems: "center",
-            position: "absolute",
-            bottom: 0,
-            left: 0,
-            width: "100%",
-            height: "80%",
-            userSelect: "none",
-            flexDirection: "column",
-            justifyContent: "flex-start",
-            pointerEvents: "auto",
-            padding: "65px",
-            color: "#fff",
-            fontFamily: "'Poppins', sans-serif",
-            transform: "translateY(25%);",
-            transition: "1.50s ease all",
-
-            "&:hover": {
-              transform: "translateY(0%);",
-              transition: "0.75s ease all",
-            },
+            fontSize: "32px",
+            fontWeight: 600,
+            fontFamily: "'Poppins', sans-serif !important",
           }}
         >
-          <Box
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              position: "absolute",
-              bottom: 0,
-              left: 0,
-              width: "100%",
-              height: "25%",
-              userSelect: "none",
-              flexDirection: "column",
-              justifyContent: "flex-start",
-              pointerEvents: "auto",
-              padding: "65px",
-              color: "#fff",
-              fontFamily: "'Poppins', sans-serif",
-              background: "rgba(255,255,255,0)",
-              backdropFilter: "blur(7px)",
-              boxShadow: "0 8px 32px rgba(31,38,135, 0.37)",
-            }}
-          >
-            <Typography
-              sx={{
-                fontSize: "32px",
-                fontWeight: 600,
-                fontFamily: "'Poppins', sans-serif !important",
+          {title}
+        </Typography>
+        <Link href={"/venture"}>
+          <a>
+            <Box
+              style={{
+                display: "flex",
+                alignItems: "center",
+                fontSize: "18px",
+                margin: "5px 0",
               }}
             >
-              {title}
-            </Typography>
-            <Link href={"/venture"}>
-              <a>
-                <Box
-                  style={{
-                    display: "flex",
-                    alignItems: "center",
-                    fontSize: "18px",
-                    margin: "5px 0",
+              <Box>
+                <Typography
+                  sx={{
+                    fontSize: "20px",
+                    fontWeight: 500,
+                    fontFamily: "'Poppins', sans-serif",
                   }}
                 >
-                  <Box>
-                    <Typography
-                      sx={{
-                        fontSize: "20px",
-                        fontWeight: 500,
-                        fontFamily: "'Poppins', sans-serif",
-                      }}
-                    >
-                      ver emprendimiento
-                    </Typography>
-                  </Box>
-                  <Box sx={{ height: "100%" }}>
-                    <KeyboardArrowRightIcon sx={{ height: "100%" }} />
-                  </Box>
-                </Box>
-              </a>
-            </Link>
-          </Box>
-        </Box>
-
-        {/* <img src={img} alt={title} /> */}
+                  ver emprendimiento
+                </Typography>
+              </Box>
+              <Box sx={{ height: "100%" }}>
+                <KeyboardArrowRightIcon sx={{ height: "100%" }} />
+              </Box>
+            </Box>
+          </a>
+        </Link>
       </Box>
     </Box>
   );
