a25094c44019e20696d16760fb7731f7a3054e5e
gregoriomartocci <gregoriomartocci@gmail.com>
2022-09-15 20:53:54 -0300

    ok

--- client/pages/home/Components/Card/index.tsx
+++ client/pages/home/Components/Card/index.tsx
@@ -17,74 +17,75 @@ const Card = ({ img, title, link }: TDemo) => {
   };
 
   return (
-    <Box
-      onMouseEnter={handleMouseEnter}
-      onMouseLeave={handleMouseLeave}
-      sx={{
-        position: "relative",
-        display: "flex",
-        cursor: "pointer",
-        fontFamily: "'Poppins', sans-serif",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100vh",
-        overflow: "hidden",
-
-        img: {
-          display: "flex",
-          position: "absolute",
-          top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
-          objectFit: "cover",
-          objectPosition: { xs: " 75% 0%" },
-          transition: "all 1.25s ease",
-
-          "&:hover ": {
-            transition: "all 1.25s ease",
-            transform: "scale(1.05)",
-          },
-        },
-      }}
-    >
-      <img src={img ? img : ""} alt={title ? title : ""} />
-
-      <Box
-        sx={{
-          display: "flex",
-          alignItems: "center",
-          position: "absolute",
-          left: 0,
-          width: "100%",
-          userSelect: "none",
-          flexDirection: "column",
-          justifyContent: "flex-start",
-          pointerEvents: "auto",
-          height: "25%",
-          padding: "65px",
-          color: "#fff",
-          fontFamily: "'Poppins', sans-serif",
-          background: "rgba(255,255,255,0)",
-          backdropFilter: "blur(7px)",
-          boxShadow: "0 8px 32px rgba(31,38,135, 0.37)",
-          bottom: `${hover ? 0 : "-25%"}`,
-          transition: "all 1.25s ease",
-          transform: "scale(1.05)",
-        }}
-      >
-        <Typography
+    <Link href={link ? link : ""}>
+      <a target="_blank">
+        <Box
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
           sx={{
-            fontSize: "32px",
-            fontWeight: 600,
-            fontFamily: "'Poppins', sans-serif !important",
+            position: "relative",
+            display: "flex",
+            cursor: "pointer",
+            fontFamily: "'Poppins', sans-serif",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100vh",
+            overflow: "hidden",
+
+            img: {
+              display: "flex",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              objectPosition: { xs: " 75% 0%" },
+              transition: "all 1.25s ease",
+
+              "&:hover ": {
+                transition: "all 1.25s ease",
+                transform: "scale(1.05)",
+              },
+            },
           }}
         >
-          {title}
-        </Typography>
-        <Link href={link ? link : ""}>
-          <a target="_blank">
+          <img src={img ? img : ""} alt={title ? title : ""} />
+
+          <Box
+            sx={{
+              display: "flex",
+              alignItems: "center",
+              position: "absolute",
+              left: 0,
+              width: "100%",
+              userSelect: "none",
+              flexDirection: "column",
+              justifyContent: "flex-start",
+              pointerEvents: "auto",
+              height: "25%",
+              padding: "65px",
+              color: "#fff",
+              fontFamily: "'Poppins', sans-serif",
+              background: "rgba(255,255,255,0)",
+              backdropFilter: "blur(7px)",
+              boxShadow: "0 8px 32px rgba(31,38,135, 0.37)",
+              bottom: `${hover ? 0 : "-25%"}`,
+              transition: "all 1.25s ease",
+              transform: "scale(1.05)",
+            }}
+          >
+            <Typography
+              sx={{
+                fontSize: "32px",
+                fontWeight: 600,
+                fontFamily: "'Poppins', sans-serif !important",
+              }}
+            >
+              {title}
+            </Typography>
+
             <Box
               style={{
                 display: "flex",
@@ -108,10 +109,10 @@ const Card = ({ img, title, link }: TDemo) => {
                 <KeyboardArrowRightIcon sx={{ height: "100%" }} />
               </Box>
             </Box>
-          </a>
-        </Link>
-      </Box>
-    </Box>
+          </Box>
+        </Box>
+      </a>
+    </Link>
   );
 };
 
