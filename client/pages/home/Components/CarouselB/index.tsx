533f1031a90154329059493101341b0fdb64b2bf
USER ITPS <gregoriomartocci@gmail.com>
2022-10-02 23:02:07 -0300

    ok

--- client/pages/home/Components/CarouselB/index.tsx
+++ client/pages/home/Components/CarouselB/index.tsx
@@ -2,9 +2,10 @@ import React, { useRef, useState } from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import "swiper/css";
 import "swiper/css/navigation";
+import styles from "../../../../styles/home.module.css";
 
 // import required modules
 
@@ -15,20 +16,82 @@ export const CarouselB = ({ items }) => {
   };
 
   return (
-    <Swiper navigation={true} modules={[Navigation]}>
+    <Swiper
+      navigation={true}
+      freeMode={true}
+      loop={true}
+      modules={[Navigation]}
+
+    >
       {items?.map((element) => {
         return (
           <SwiperSlide>
-            
-            <img
-              style={{
-                width: "100vw",
-                height: "100vh",
-                objectFit: "cover",
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
               }}
-              src={element?.src}
-              alt="foto"
-            />
+            >
+              <Typography
+                sx={{
+                  position: "absolute",
+                  padding: "0 10%",
+                  fontSize: "38px",
+                  fontWeight: "500",
+                  color: "#fff",
+                  zIndex: 20,
+                  textAlign: "center",
+                }}
+              >
+                {element?.phrase}
+              </Typography>
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: 0,
+                  left: 0,
+                  display: "flex",
+                  justifyContent: "center",
+                  alignItems: "center",
+                  width: "100%",
+                  height: "100%",
+                  backgroundColor: "#212121",
+                  zIndex: 15,
+                  pointerEvents: "none",
+                  opacity: "0.50",
+                }}
+              />
+
+              {getFormat(element?.src) === "MP4" ? (
+                <video
+                  style={{
+                    width: "100vw",
+                    height: "100vh",
+                    objectFit: "cover",
+                  }}
+                  src={element?.src}
+                  autoPlay
+                  loop
+                  muted
+                />
+              ) : (
+                <img
+                  style={{
+                    width: "100vw",
+                    height: "100vh",
+                    objectFit: "cover",
+                  }}
+                  src={element?.src}
+                  alt="foto"
+                />
+              )}
+            </Box>
           </SwiperSlide>
         );
       })}
