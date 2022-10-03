5b3c44a63cca7cb2f77d70ba7640c267cfdab34a
USER ITPS <gregoriomartocci@gmail.com>
2022-10-02 21:29:41 -0300

    ok

--- client/pages/home/Components/CarouselB/index.tsx
+++ client/pages/home/Components/CarouselB/index.tsx
@@ -1,5 +1,4 @@
 import React, { useRef, useState } from "react";
-
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper";
@@ -10,7 +9,6 @@ import "swiper/css/navigation";
 // import required modules
 
 export const CarouselB = ({ items }) => {
-
   const getFormat = (file: string) => {
     const result = file?.split(".").pop()?.toUpperCase();
     return result;
@@ -21,29 +19,16 @@ export const CarouselB = ({ items }) => {
       {items?.map((element) => {
         return (
           <SwiperSlide>
-            {getFormat(element?.src) !== "MP4" ? (
-              <video
-                style={{
-                  width: "100vw",
-                  height: "100vh",
-                  objectFit: "cover",
-                }}
-                src={element?.src}
-                autoPlay
-                loop
-                muted
-              />
-            ) : (
-              <img
-                style={{
-                  width: "100%",
-                  height: "100%",
-                  objectFit: "cover",
-                }}
-                src={element?.src}
-                alt="foto"
-              />
-            )}
+            
+            <img
+              style={{
+                width: "100vw",
+                height: "100vh",
+                objectFit: "cover",
+              }}
+              src={element?.src}
+              alt="foto"
+            />
           </SwiperSlide>
         );
       })}
