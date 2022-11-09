35d1cdf779e6b590ea05e0a64b595998c6ae846d
Gregorio Martocci <gregoriomartocci@gmail.com>
2022-11-09 14:58:51 -0300

    ok

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ mongoose
   .catch((err) => console.log("DB CONNECTION ERROR: ", err));
 
 // middlewares
-app.use(express.json({ limit: "50mb" }));
+app.use(express.json({ limit: "80mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 app.use(morgan("dev"));
